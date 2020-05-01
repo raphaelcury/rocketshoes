bbcb93ce68b7baf9ca05e63df41106049e379d05
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-01 16:46:25 -0300

    Cart stylization.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable } from './styles';
 
 export default function Cart() {
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <th> </th>
+          <th>PRODUTO</th>
+          <th>QTD</th>
+          <th>SUBTOTAL</th>
+          <th> </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="productImage">
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td className="product">
+              <div>
+                <strong>Asics Gel Kayano 25</strong>
+                <span>R$459,90</span>
+              </div>
+            </td>
+            <td className="qtd">
+              <div>
+                <button type="button">
+                  <MdAddCircleOutline size={20} />
+                </button>
+                <input type="text" name="qtd" id="qtd" value="2" />
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} />
+                </button>
+              </div>
+            </td>
+            <td className="subtotal">
+              <strong>R$919,80</strong>
+            </td>
+            <td className="delete">
+              <button type="button">
+                <MdDelete size={20} />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="submit">Finalizar Pedido</button>
+        <div>
+          <span>Total</span>
+          <strong>R$919,80</strong>
+        </div>
+      </footer>
+    </Container>
   );
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+
+  footer {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    button {
+      display: flex;
+      align-items: center;
+
+      background: darkgray;
+      color: black;
+      font-weight: bold;
+      padding: 12px;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      margin-top: auto;
+      transition: 0.2s;
+      text-transform: uppercase;
+
+      &:hover {
+        background: ${darken(0.03, '#A9A9A9')};
+      }
+    }
+
+    div {
+      display: flex;
+      align-items: baseline;
+      span {
+        font-size: 12px;
+        color: #999;
+        margin-right: 5px;
+        text-transform: uppercase;
+      }
+      strong {
+        font-size: 20px;
+      }
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+  border-bottom: 2px solid gainsboro;
+
+  thead th {
+    font-size: 14px;
+    color: #999;
+    text-align: left;
+  }
+
+  tbody td {
+    text-align: left;
+  }
+
+  tbody td.productImage {
+    img {
+      height: 120px;
+    }
+  }
+
+  tbody td.product {
+    div {
+      display: flex;
+      flex-direction: column;
+      strong {
+        color: #333;
+        font-size: 14px;
+        line-height: 20px;
+      }
+      span {
+        font-size: 18px;
+        font-weight: bold;
+      }
+    }
+  }
+
+  tbody td.qtd {
+    div {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      button {
+        display: flex;
+        border: 0;
+        background: none;
+        color: darkgray;
+        padding: 6px;
+      }
+      input {
+        width: 50px;
+        border: 1px solid lightgray;
+        border-radius: 4px;
+        padding: 5px;
+      }
+    }
+  }
+
+  tbody td.delete {
+    button {
+      border: 0;
+      background: none;
+      color: darkgray;
+    }
+  }
+`;
