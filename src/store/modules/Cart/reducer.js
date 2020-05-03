a9bae59a7ec17b5e8ffa1ed17d728416f822ed66
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-03 17:33:11 -0300

    Add 1 in qty when add same product to cart.

--- src/store/modules/Cart/reducer.js
+++ src/store/modules/Cart/reducer.js
@@ -1,13 +1,20 @@
 export default function cart(state = [], action) {
   switch (action.type) {
-    case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
+    case 'ADD_TO_CART': {
+      const newCart = [...state];
+      const productIndex = state.findIndex(
+        (product) => product.id === action.product.id
+      );
+      if (productIndex >= 0) {
+        newCart[productIndex].amount += 1;
+      } else {
+        newCart.push({
           ...action.product,
           amount: 1,
-        },
-      ];
+        });
+      }
+      return newCart;
+    }
     default:
       return state;
   }
