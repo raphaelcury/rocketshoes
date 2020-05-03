b0496810b3ea62cdaabc922a40c021c5ec81c774
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-03 10:49:27 -0300

    Getting product list from api.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,112 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>
-          Tênis Asics Gel Kayano 25 o que será que vai acontecer quando quebrar
-          a linha?
-        </strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Asics Gel Kayano 25</strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Asics Gel Kayano 25</strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Asics Gel Kayano 25</strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Asics Gel Kayano 25</strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Asics Gel Kayano 25</strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-kayano-25-feminino/18/D18-2869-118/D18-2869-118_zoom2.jpg?ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis Asics Gel Kayano 25</strong>
-        <span>R$ 459,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart /> <span>3</span>
-          </div>
-          <strong>Adicionar ao carrinho</strong>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const products = await api.get('/products');
+    console.log(products);
+    this.setState({ products: products.data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt="Tênis" />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} /> <span>3</span>
+              </div>
+              <strong>Adicionar ao carrinho</strong>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
