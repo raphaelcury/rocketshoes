61b26d44e15262a45329121d012fe9c6354a1312
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-03 17:54:17 -0300

    Qty modification in reducer using immer.

--- src/store/modules/Cart/reducer.js
+++ src/store/modules/Cart/reducer.js
@@ -1,20 +1,21 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
-    case 'ADD_TO_CART': {
-      const newCart = [...state];
-      const productIndex = state.findIndex(
-        (product) => product.id === action.product.id
-      );
-      if (productIndex >= 0) {
-        newCart[productIndex].amount += 1;
-      } else {
-        newCart.push({
-          ...action.product,
-          amount: 1,
-        });
-      }
-      return newCart;
-    }
+    case 'ADD_TO_CART':
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex(
+          (product) => product.id === action.product.id
+        );
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
