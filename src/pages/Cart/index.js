7bacecda95a7b5771d334fc0333be8cb53b1cc28
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-04 19:56:20 -0300

    Delete button on cart page.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   MdAddCircleOutline,
   MdRemoveCircleOutline,
@@ -8,66 +8,80 @@ import { connect } from 'react-redux';
 
 import { Container, ProductTable } from './styles';
 
-function Cart({ cart }) {
-  return (
-    <Container>
-      <ProductTable>
-        <thead>
-          <th> </th>
-          <th>PRODUTO</th>
-          <th>QTD</th>
-          <th>SUBTOTAL</th>
-          <th> </th>
-        </thead>
-        <tbody>
-          {cart.map((product) => (
-            <tr>
-              <td className="productImage">
-                <img src={product.image} alt="Tênis" />
-              </td>
-              <td className="product">
-                <div>
-                  <strong>{product.title}</strong>
-                  <span>{product.formattedPrice}</span>
-                </div>
-              </td>
-              <td className="qtd">
-                <div>
-                  <button type="button">
-                    <MdAddCircleOutline size={20} />
+class Cart extends Component {
+  handleDeleteButton = (productId) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'REMOVE_FROM_CART',
+      productId,
+    });
+  };
+
+  render() {
+    const { cart } = this.props;
+    return (
+      <Container>
+        <ProductTable>
+          <thead>
+            <th> </th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th> </th>
+          </thead>
+          <tbody>
+            {cart.map((product) => (
+              <tr>
+                <td className="productImage">
+                  <img src={product.image} alt="Tênis" />
+                </td>
+                <td className="product">
+                  <div>
+                    <strong>{product.title}</strong>
+                    <span>{product.formattedPrice}</span>
+                  </div>
+                </td>
+                <td className="qtd">
+                  <div>
+                    <button type="button">
+                      <MdAddCircleOutline size={20} />
+                    </button>
+                    <input
+                      type="text"
+                      name="qtd"
+                      id="qtd"
+                      value={product.amount}
+                    />
+                    <button type="button">
+                      <MdRemoveCircleOutline size={20} />
+                    </button>
+                  </div>
+                </td>
+                <td className="subtotal">
+                  <strong>R$919,80</strong>
+                </td>
+                <td className="delete">
+                  <button
+                    type="button"
+                    onClick={() => this.handleDeleteButton(product.id)}
+                  >
+                    <MdDelete size={20} />
                   </button>
-                  <input
-                    type="text"
-                    name="qtd"
-                    id="qtd"
-                    value={product.amount}
-                  />
-                  <button type="button">
-                    <MdRemoveCircleOutline size={20} />
-                  </button>
-                </div>
-              </td>
-              <td className="subtotal">
-                <strong>R$919,80</strong>
-              </td>
-              <td className="delete">
-                <button type="button">
-                  <MdDelete size={20} />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </ProductTable>
-      <footer>
-        <button type="submit">Finalizar Pedido</button>
-        <div>
-          <span>Total</span>
-          <strong>R$919,80</strong>
-        </div>
-      </footer>
-    </Container>
-  );
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </ProductTable>
+        <footer>
+          <button type="submit">Finalizar Pedido</button>
+          <div>
+            <span>Total</span>
+            <strong>R$919,80</strong>
+          </div>
+        </footer>
+      </Container>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
--- src/store/modules/Cart/reducer.js
+++ src/store/modules/Cart/reducer.js
@@ -16,6 +16,15 @@ export default function cart(state = [], action) {
           });
         }
       });
+    case 'REMOVE_FROM_CART':
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex(
+          (product) => product.id === action.productId
+        );
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
     default:
       return state;
   }
