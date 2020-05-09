922953b5b803e1d32930bbf08d327ccbc3167810
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-09 15:54:41 -0300

    Refactoring add product between reducer and saga.

--- src/store/modules/Cart/sagas.js
+++ src/store/modules/Cart/sagas.js
@@ -1,10 +1,23 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateProductAmount } from './actions';
 
 function* addToCart({ productId }) {
-  const response = yield call(api.get, `/products/${productId}`);
-  yield put(addToCartSuccess(response.data));
+  const existingProduct = yield select((state) =>
+    state.cart.find((product) => product.id === productId)
+  );
+  if (!existingProduct) {
+    const response = yield call(api.get, `/products/${productId}`);
+    const productData = {
+      ...response.data,
+      amount: 1,
+    };
+    yield put(addToCartSuccess(productData));
+  } else {
+    yield put(
+      updateProductAmount(existingProduct.id, existingProduct.amount + 1)
+    );
+  }
 }
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
