6d6c68b9e5dbd31af58c2999ba056c36348033ac
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-23 19:40:58 -0300

    Added useSelector hook to header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-function Header({ cartSize }) {
+export default function Header() {
+  const cartSize = useSelector((state) => state.cart.length);
   return (
     <Container>
       <Link to="/">
@@ -24,9 +24,3 @@ function Header({ cartSize }) {
     </Container>
   );
 }
-
-Header.propTypes = {
-  cartSize: PropTypes.number.isRequired,
-};
-
-export default connect((state) => ({ cartSize: state.cart.length }))(Header);
